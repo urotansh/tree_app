ba919dff78819e29b5f62cb321c4d597474c1256
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-16 16:57:14 +0900

    Add bootstrap/jquery/popper settings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,9 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application" 
+import "../stylesheets/scaffolds"
